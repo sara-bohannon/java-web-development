43c0491c018f17c783dcfb8b2c16fe533d5bbe2d
Sara Bohannon <your.email@example.com>
2024-09-30 17:52:05 -0500

    studio

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        String testString = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+
+        char[] lettersOfString = testString.toCharArray() ;
+
+        HashMap<Character, Integer> countOfLetters = new HashMap<>();
+        for(char letter: lettersOfString){
+            if (countOfLetters.containsKey(letter)) {
+                countOfLetters.put(letter, countOfLetters.get(letter) + 1);
+            } else {
+                countOfLetters.put(letter, 1);
+            }
+        }
+        for(Map.Entry<Character, Integer> pair : countOfLetters.entrySet()){
+            System.out.println(pair.getKey() + " : " + pair.getValue());
+        }
+    }
+
+}
